203d069657c54609d41c317c47703f0177399e80
Gustavo Gialluca <30830706+gustavogialluca@users.noreply.github.com>
2021-11-30 17:42:04 -0300

    Add files via upload

---
+++ MPU6050/AI/accel_inference.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+import pickle
+from math import pi
+
+# Read data from txt file, transform Gyro data into degrees/second and store it into pandas dataframe
+df = pd.read_csv("E:\\Gustavo\\GENERA\\Direção Segura\\MPU6050\\dados\\testes_dia_2\\4_retorno_esquerda_brusco.txt")
+print(df.head())
+df.iloc[:, -3:] = df.iloc[:,-3:].apply(lambda x: 180*x/pi)
+print(df.head())
+
+# columns = ['AccMeanX','AccMeanY','AccMeanZ','AccCovX','AccCovY','AccCovZ','AccSkewX','AccSkewY','AccSkewZ','AccKurtX','AccKurtY','AcKurtZ','AccSumX','AccSumY','AccSumZ','AccMinX','AccMinY','AccMinZ',
+#             'AccMaxX','AccMaxY','AccMaxZ','AccVarX','AccVarY','AccVarZ','AccMedianX','AccMedianY','AccMedianZ','AccStdX','AccStdY','AccStdZ','GyroMeanX','GyroMeanY','GyroMeanZ','GyroCovX','GyroCovY','GyroCovZ',
+#                 'GyroSkewX','GyroSkewY','GyroSkewZ','GyroSumX','GyroSumY','GyroSumZ','GyroKurtX','GyroKurtY','GyroKurtZ','GyroMinX','GyroMinY','GyroMinZ','GyroMaxX','GyroMaxY','GyroMaxZ','GyroVarX','GyroVarY',
+#                     'GyroVarZ','GyroMedianX','GyroMedianY','GyroMedianZ','GyroStdX','GyroStdY','GyroStdZ']
+
+columns = ['AccMeanX','AccMeanY','AccMeanZ','AccSkewX','AccSkewY','AccSkewZ','AccKurtX','AccKurtY','AcKurtZ','AccSumX','AccSumY','AccSumZ','AccMinX','AccMinY','AccMinZ',
+            'AccMaxX','AccMaxY','AccMaxZ','AccVarX','AccVarY','AccVarZ','AccMedianX','AccMedianY','AccMedianZ','AccStdX','AccStdY','AccStdZ','GyroMeanX','GyroMeanY','GyroMeanZ',
+                'GyroSkewX','GyroSkewY','GyroSkewZ','GyroSumX','GyroSumY','GyroSumZ','GyroKurtX','GyroKurtY','GyroKurtZ','GyroMinX','GyroMinY','GyroMinZ','GyroMaxX','GyroMaxY','GyroMaxZ',
+                    'GyroVarX','GyroVarY','GyroVarZ','GyroMedianX','GyroMedianY','GyroMedianZ','GyroStdX','GyroStdY','GyroStdZ']
+
+list_input = []
+
+# Acceleration
+list_input.append(df['ax'].mean())
+list_input.append(df['ay'].mean())
+list_input.append(df['az'].mean())
+
+# list_input.append(df['ax'].cov(other=df['ax']))
+# list_input.append(df['ay'].cov(other=df['az']))
+# list_input.append(df['az'].cov(other=df['ax']))
+
+list_input.append(df['ax'].skew())
+list_input.append(df['ay'].skew())
+list_input.append(df['az'].skew())
+
+list_input.append(df['ax'].kurt())
+list_input.append(df['ay'].kurt())
+list_input.append(df['az'].kurt())
+
+list_input.append(df['ax'].sum())
+list_input.append(df['ay'].sum())
+list_input.append(df['az'].sum())
+
+list_input.append(df['ax'].min())
+list_input.append(df['ay'].min())
+list_input.append(df['az'].min())
+
+list_input.append(df['ax'].max())
+list_input.append(df['ay'].max())
+list_input.append(df['az'].max())
+
+list_input.append(df['ax'].var())
+list_input.append(df['ay'].var())
+list_input.append(df['az'].var())
+
+list_input.append(df['ax'].median())
+list_input.append(df['ay'].median())
+list_input.append(df['az'].median())
+
+list_input.append(df['ax'].std())
+list_input.append(df['ay'].std())
+list_input.append(df['az'].std())
+
+# Gyro
+list_input.append(df['gx'].mean())
+list_input.append(df['gy'].mean())
+list_input.append(df['gz'].mean())
+
+# list_input.append(df['gx'].cov())
+# list_input.append(df['gy'].cov())
+# list_input.append(df['gz'].cov())
+
+list_input.append(df['gx'].skew())
+list_input.append(df['gy'].skew())
+list_input.append(df['gz'].skew())
+
+list_input.append(df['gx'].kurt())
+list_input.append(df['gy'].kurt())
+list_input.append(df['gz'].kurt())
+
+list_input.append(df['gx'].sum())
+list_input.append(df['gy'].sum())
+list_input.append(df['gz'].sum())
+
+list_input.append(df['gx'].min())
+list_input.append(df['gy'].min())
+list_input.append(df['gz'].min())
+
+list_input.append(df['gx'].max())
+list_input.append(df['gy'].max())
+list_input.append(df['gz'].max())
+
+list_input.append(df['gx'].var())
+list_input.append(df['gy'].var())
+list_input.append(df['gz'].var())
+
+list_input.append(df['gx'].median())
+list_input.append(df['gy'].median())
+list_input.append(df['gz'].median())
+
+list_input.append(df['gx'].std())
+list_input.append(df['gy'].std())
+list_input.append(df['gz'].std())
+
+# Transform list of data into pandas dataframe
+df_input = pd.DataFrame([list_input])
+print(df_input.head())
+
+# Scale data and load model to make predictions
+scaler = MinMaxScaler(feature_range=(-1,1))
+X_test_scaled = scaler.fit_transform(df_input)
+filename = 'lr_classifier_inertial.sav'
+model = pickle.load(open(filename, 'rb'))
+prediction = model.predict(X_test_scaled)
+prediction_prob = model.predict_proba(X_test_scaled)
+print("PREDICTION: " + str(prediction))
+print("PREDICTION PROBABILITY: " + str(prediction_prob))
---
+++ MPU6050/AI/model_train.py
@@ -0,0 +1,44 @@
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import pickle
+
+# Load dataset and store it in pandas dataframe and print first 5 rows
+DATASET_PATH = "E:\\Gustavo\\GENERA\\Direção Segura\\MPU6050\\AI\\mendeley_data\\Features By Window Size\\features_14.csv"
+df = pd.read_csv(DATASET_PATH)
+print(df.head())
+
+# Separate data into train and test
+train_data, test_data = train_test_split(df, random_state=12, test_size=0.25)
+X_train = train_data.drop(['Target', 'AccCovX', 'AccCovY', 'AccCovZ', 'GyroCovX', 'GyroCovY', 'GyroCovZ'], axis='columns')
+y_train = train_data['Target']
+X_test = test_data.drop(['Target', 'AccCovX', 'AccCovY', 'AccCovZ', 'GyroCovX', 'GyroCovY', 'GyroCovZ'], axis='columns')
+y_test = test_data['Target']
+print("X_train shape: " + str(X_train.shape))
+print("y_train shape: " + str(y_train.shape))
+print("X_test shape: " + str(X_test.shape))
+print("y_test shape: " + str(y_test.shape))
+
+# Load model from sklearn, standardize data and fit it to the train data
+from sklearn.svm import SVC
+# from sklearn.linear_model import LogisticRegression
+scaler = MinMaxScaler(feature_range=(-1,1))
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+model = SVC(gamma='auto', kernel='linear')
+# model = LogisticRegression()
+model.fit(X_train_scaled, y_train)
+filename = 'svc_classifier_inertial.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+# Make predictions and evaluate model
+y_pred = model.predict(X_test_scaled)
+print(confusion_matrix(y_test, y_pred))
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+# sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+# plt.show()
