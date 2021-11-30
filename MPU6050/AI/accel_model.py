f7c4c863fa2b2d808f3a6803f4e2db3ba1a40a1d
Gustavo Gialluca <30830706+gustavogialluca@users.noreply.github.com>
2021-11-30 10:17:24 -0300

    Update accel_model.py

--- MPU6050/AI/accel_model.py
+++ MPU6050/AI/accel_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # Read data from txt file and store it into pandas dataframe
-df = pd.read_csv("E:\\Gustavo\\GENERA\\Direção Segura\\MPU6050\\dados\\testes_dia_2\\15_desaceleracao_brusca.txt")
+df = pd.read_csv("PATH_TO_TXT_FILE")
 print(df.head())
 
 columns = ['Target','AccMeanX','AccMeanY','AccMeanZ','AccCovX','AccCovY','AccCovZ','AccSkewX','AccSkewY','AccSkewZ','AccKurtX','AccKurtY','AcKurtZ','AccSumX','AccSumY','AccSumZ','AccMinX','AccMinY','AccMinZ',
